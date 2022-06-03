f2c3cab47a28b96429ead52283b9867d75040fc4
yaroslavLarionov <yarillo@yahoo.com>
2022-06-03 12:35:45 -0400

    created test method to cover US-03

--- src/test/pages/UserManagementPage.java
+++ src/test/pages/UserManagementPage.java
@@ -20,5 +20,29 @@ public class UserManagementPage extends BasePage {
 
     public String expectedTitle = "User DB";
 
+    @FindBy (id = "Firstname")
+    public WebElement firstNameEl;
+
+    @FindBy (id = "Lastname")
+    public WebElement lastNameEl;
+
+    @FindBy (id = "Phonenumber")
+    public WebElement phoneNumberEl;
+
+    @FindBy (id = "Email")
+    public WebElement emailEl;
+
+    @FindBy (id = "Select-role")
+    public WebElement roleID;
+
+    @FindBy (id = "submit-btn")
+    public WebElement submitBtn;
+
+    @FindBy (id = "submit-table-btn")
+    public WebElement submitTableBtn;
+
+    @FindBy (xpath = "(//table//tr)[last()]")
+    public WebElement lastRow;
+
 
 }
--- src/test/tests/UserManagementTest.java
+++ src/test/tests/UserManagementTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import data.dataProviders.DataProviders;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -28,6 +29,32 @@ public class UserManagementTest extends BaseTest {
         seleniumUtils.switchToNextWindow(getDriver());
         extentManager.logScreenshot();
         Assert.assertEquals(getDriver().getTitle(), userManagementPage.expectedTitle);
+    }
+
+    @Test (testName = "US-03", description = "Verify new user is created in DB", dataProvider = "Role info", dataProviderClass = DataProviders.class)
+    public void test03(String firstName, String lastName, String phone, String email, String role) {
+        homePage.userMgtBtn.click();
+        //create new user information
+        userManagementPage.firstNameEl.sendKeys(firstName);
+        userManagementPage.lastNameEl.sendKeys(lastName);
+        userManagementPage.phoneNumberEl.sendKeys(phone);
+        userManagementPage.emailEl.sendKeys(email);
+        userManagementPage.roleID.sendKeys(role);
+        //click submit the info for each user and then click submit the table
+        userManagementPage.submitBtn.click();
+        userManagementPage.submitTableBtn.click();
+        //access the DB, make a screenshot and verify the user is there
+        userManagementPage.accessDbBtn.click();
+        seleniumUtils.switchToNextWindow(getDriver());
+        userManagementPage.waitForElementVisibility(userManagementPage.lastRow);
+        userManagementPage.moveIntoView(userManagementPage.lastRow);
+        extentManager.logScreenshot();
+        Assert.assertTrue(userManagementPage.lastRow.getText().contains(firstName) &&
+                        userManagementPage.lastRow.getText().contains(lastName) &&
+                        userManagementPage.lastRow.getText().contains(phone) &&
+                        userManagementPage.lastRow.getText().contains(email) &&
+                        userManagementPage.lastRow.getText().contains(role));
+
     }
 
 
